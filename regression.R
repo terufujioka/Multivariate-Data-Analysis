2c633c416827a2be18e689a7a11da2046b54a444
terufujioka <terufujioka3522@gmail.com>
2025-12-12 09:57:16 +0900

    回帰の結果のsummaryを追加

--- regression.R
+++ regression.R
@@ -1,5 +1,7 @@
 mydata     <- read.csv("data.csv")
 reg.output <- lm(y ~ x, mydata)
 
-# To do: Study the summary from reg.output
-  
+# Summary:
+summary(reg.output)
+# I can see that R-squared is 77.1%, and the regression coefficient of x is 
+#   significantly different from 0 at alpha = 5%.
